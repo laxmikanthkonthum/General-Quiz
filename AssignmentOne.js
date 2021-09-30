ca8239cfdc7b52e36b84eaed83c5f0640f5319ae
laxmikanthkonthum <43950535+laxmikanthkonthum@users.noreply.github.com>
2021-10-01 00:10:04 +0530

    Update: Added two questions

--- AssignmentOne.js
+++ AssignmentOne.js
@@ -15,7 +15,7 @@ if(userAnswer.toLowerCase() == answer.toLowerCase()){
   console.log(chalk.redBright("Thats wrong\n" + chalk.cyan("The right answer is " + chalk.green(answer) + '\n')))
 }
 }
-var queArr = [{question: "Which is the longest river in the world? ",answer: "Nile"},{question:"What is the middle name of Mahatma Gandhi? ", answer: "Karamchand"},{question: "The National Stock Exchange of India is located at?", answer: "Mumbai"}];
+var queArr = [{question: "Which is the longest river in the world? ",answer: "Nile"},{question:"What is the middle name of Mahatma Gandhi? ", answer: "Karamchand"},{question: "The National Stock Exchange of India is located at?", answer: "Mumbai"},{question: "Where is Supreme Court of India?", answer: "Delhi"},{question: "What is finance capital of India?", answer: "Mumbai"}];
 for(i=0;i<queArr.length;i++){
   console.log("Here is your question no: " + (i+1))
   quiz(queArr[i].question,queArr[i].answer);
